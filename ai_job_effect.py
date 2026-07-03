aaeb8b035b0e47ea5238ba36f6e7e0de28063b09
KRIZNA GARG <krishna garg222008@gmail.com>
2026-07-03 19:49:53 +0530

    no. of unique jobs and their names

--- ai_job_effect.py
+++ ai_job_effect.py
@@ -10,3 +10,15 @@ data=pd.read_csv("ai_job_trends_dataset.csv")
 print(data.head())
 print(data.shape)
 
+print(data.isnull().sum())
+print(data.duplicated().sum())
+
+#According to output no null value nor any duplicated one
+
+#no.of unique jobs and their names in which ai is effecting:
+number_of_unique_jobs=data['Industry'].nunique()
+unique_jobs=np.unique(data['Industry'])
+print("number of unique jobs are:",number_of_unique_jobs,"and their names are:-\n",unique_jobs)
+#output:-['Education', 'Entertainment', 'Finance' ,'Healthcare' ,'IT' ,'Manufacturing','Retail','Transportation']-total 8
+
+
