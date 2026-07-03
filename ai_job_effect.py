a38dcb789efea3e901305578f128e10594fb73b2
KRIZNA GARG <krishna garg222008@gmail.com>
2026-07-03 19:25:45 +0530

    data cleaning

---
+++ ai_job_effect.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#loading dataset
+data=pd.read_csv("ai_job_trends_dataset.csv")
+
+#Data cleaning
+print(data.head())
+print(data.shape)
+
