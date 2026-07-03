5d3cbe1537fa69d5a2e19a3ced905b5d780ef93d
KRIZNA GARG <krishna garg222008@gmail.com>
2026-07-03 19:50:34 +0530

    top 5 industries in which job status is increasing after ai

--- ai_job_effect.py
+++ ai_job_effect.py
@@ -16,9 +16,12 @@ print(data.duplicated().sum())
 #According to output no null value nor any duplicated one
 
 #no.of unique jobs and their names in which ai is effecting:
-number_of_unique_jobs=data['Industry'].nunique()
-unique_jobs=np.unique(data['Industry'])
-print("number of unique jobs are:",number_of_unique_jobs,"and their names are:-\n",unique_jobs)
+# number_of_unique_jobs=data['Industry'].nunique()
+# unique_jobs=np.unique(data['Industry'])
+# print("number of unique jobs are:",number_of_unique_jobs,"and their names are:-\n",unique_jobs)
 #output:-['Education', 'Entertainment', 'Finance' ,'Healthcare' ,'IT' ,'Manufacturing','Retail','Transportation']-total 8
 
+#top 5 industries in which Job status is increasing after AI:-
+industries=data[data['Job Status']=='Increasing']['Industry'].head(10)
+print(industries)
 
